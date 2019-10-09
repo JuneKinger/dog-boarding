5dd4464bbbaa665b0321367b370b97653131b267
junekinger <junekinger@hotmail.com>
2019-10-09 15:57:19 -0500

    DayOfWeek checkbox not working, otherwise dog name checkbox validation and dates validation is working!

--- src/main/java/org/launchcode/controllers/ServiceController.java
+++ src/main/java/org/launchcode/controllers/ServiceController.java
@@ -37,21 +37,16 @@ public class ServiceController {
     @RequestMapping(value = "")
     public String index(Model model, @CookieValue(value = "person",
             defaultValue = "none") String email) {
-
         if (email.equals("none")) {
             return "redirect:/person/login";
         }
-
         Person pers = personDao.findByEmail(email);
         List<Dog> dogs = pers.getDogs();
-
         // findAll() is an iterable that Thymeleaf uses in th:each="dog : ${dogs}"
         model.addAttribute("dogs", dogs);
         model.addAttribute("title", "My Dogs");
-
         return "service/add-service";
     }
-
 */
 
     @RequestMapping(value = "add-service", method = RequestMethod.GET)
@@ -79,38 +74,32 @@ public class ServiceController {
 
     @RequestMapping(value = "add-service", method = RequestMethod.POST)
     public String processAddServiceForm(@ModelAttribute @Valid Service newService, Errors errors,
-                                        @RequestParam("action") String action, int[] dogIds, String personEmail, String Radio, String dayOfWeek, Model model) {
+                                        @RequestParam("action") String action, int[] dogIds, String email, String Radio, String dayOfWeek, Model model) {
 
 /*
       if (Radio.equals("weekly") && !dayOfWeek.matches("[A-Za-z]")) {
-
           model.addAttribute("errors", "Please enter Days of the Week");
           Person pers = personDao.findByEmail(personEmail);
           List<Dog> dogs = pers.getDogs();
-
           model.addAttribute("dogs", dogs);
           model.addAttribute("person", pers);
           return "service/add-service";
           }
-
         if (dogIds == null) {
-
             model.addAttribute("errors", "Please check mark at least one dog");
             return "service/add-service";
         }
 */
-      if (errors.hasErrors()) {
+        if (errors.hasErrors()) {
             model.addAttribute("title", "Add Service");
 
             //model.addAttribute("error", "Invalid input - please reenter");
-            /*
+
             Person pers = personDao.findByEmail(email);
             List<Dog> dogs = pers.getDogs();
 
-            model.addAttribute("dogs", dogs);
+            model.addAttribute("dogs", pers.getDogs());
             model.addAttribute("person", pers);
-
-             */
             return "service/add-service";
         }
 
@@ -124,70 +113,48 @@ public class ServiceController {
                 service.setDayOfWeek(newService.getDayOfWeek());
             }
 
-            service.setPerson(personDao.findByEmail(personEmail));
+            service.setPerson(personDao.findByEmail(email));
             service.setStartDate(newService.getStartDate());
             service.setEndDate(newService.getEndDate());
 
             serviceDao.save(service);
         }
-            if (action.equals("Add")) {
-                return "redirect:/service/add-service";
-            } else {
+        if (action.equals("Add")) {
+            return "redirect:/service/add-service";
+        } else {
 
-                return "person/index";
-            }
-
+            return "person/index";
         }
-/*
 
+    }
+/*
     @RequestMapping(value = "remove-service/{id}", method = RequestMethod.GET)
     public String displayRemoveDogDetailsForm(@PathVariable int id, Model model, @CookieValue(value = "person",
             defaultValue = "none") String email) {
-
         if (email.equals("none")) {
             return "redirect:/person/login";
         }
-
         Service service = serviceDao.findById(id);
-
         //List<Dog> dogs = service.getDog();
-
         //pers.setDogs(dogs);
-
         //Dog dog = dogDao.findById(id);
-
         //model.addAttribute("person", personDao.findByEmail(email));
         //model.addAttribute("dogs", dogDao.findAll());
-
         model.addAttribute("service", serviceDao.findById(id));
-
         return "service/remove";
     }
-
-
     @RequestMapping(value = "remove-service/{id}", method = RequestMethod.POST)
     public String processRemoveDogDetailForm(@ModelAttribute @Valid Dog newDog, Errors errors,
                                              @PathVariable int id, Person person, Model model) {
-
-
         Person pers = personDao.findByEmail(person.getEmail());
-
         List<Dog> dogs = pers.getDogs();
-
         pers.setDogs(dogs);
-
         Dog dog = dogDao.findById(id);
         dog.setPerson(pers);
-
         dogDao.delete(dog);
-
         model.addAttribute("dogs", pers.getDogs());
         model.addAttribute("person", pers);
-
         return "dogs/list-dog-details";
     }
-
  */
 }
-
-
