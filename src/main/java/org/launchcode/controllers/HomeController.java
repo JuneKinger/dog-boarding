7b3ed48839568c44edd5f9ab003fbd94bb279fb9
junekinger <junekinger@hotmail.com>
2019-10-30 22:14:24 -0500

    removed extra HomeController commented-out code

--- src/main/java/org/launchcode/controllers/HomeController.java
+++ src/main/java/org/launchcode/controllers/HomeController.java
@@ -1,11 +1,8 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.data.PersonDao;
-import org.launchcode.models.forms.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,17 +14,7 @@ public class HomeController {
     @RequestMapping(value = "")
     // bind this method parameter to a cookie. Retrieve a cookie using @CookieValue
     public String index() {
-            /*
-            (Model model, @CookieValue(value="person", defaultValue = "none") String email) {
 
-        // if no cookies present, direct to login page to force a log in
-        if (email.equals("none") || email.equals("")) {
-            return "home/index";
-        }
-
-         Person person = personDao.findByEmail(email);
-         model.addAttribute("person", person.getFirstName());
-*/
         return "home/index";
     }
 }
